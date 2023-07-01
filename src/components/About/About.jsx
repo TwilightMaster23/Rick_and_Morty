e8d256936e74b4509e9d2e3293c3c1a4edec8d6f
Brian <115763338+TwilightMaster23@users.noreply.github.com>
2023-07-01 00:33:03 -0300

    Delete src directory

--- src/components/About/About.jsx
+++
@@ -1,9 +0,0 @@
-import style from './About.module.css'
-
-export default function About () {
-    return (
-        <div className={style.container}>
-        <h1 className={style.container}>About en Mantenimiento</h1>
-        </div>
-    )
-}
--- src/components/Error404/Error404.jsx
+++
@@ -1,7 +0,0 @@
-export default function Error404() {
-    return(
-        <div>
-            <img src="https://retouchcenter.org/wp-content/uploads/2019/04/error-4044.jpg" alt="Error" />
-        </div>
-    )
-}
